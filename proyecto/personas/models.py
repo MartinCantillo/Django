8375635e7e04c7e5b638716e9122bb44fea8e4bc
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-01-05 19:23:59 -0500

    adding str Persona

--- proyecto/personas/models.py
+++ proyecto/personas/models.py
@@ -5,3 +5,6 @@ class Persona(models.Model):
     nombre= models.CharField(max_length=255)
     apellido= models.CharField(max_length =255)
     email= models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f'Persona: Id ={self.id},  Nombre= {self.nombre}, Apellido= {self.apellido}, Email={self.email}'
